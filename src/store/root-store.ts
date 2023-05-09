eb1a407fc8b58b6e8ba6180b3bcb1bc0a4e415b3
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-05-09 10:52:12 +0300

    feat: adding auth store

---
+++ src/store/root-store.ts
@@ -0,0 +1,21 @@
+import {create} from "zustand";
+import {AuthInfos} from "@/models/Auth";
+
+type State = {
+    user: AuthInfos
+}
+
+type Action = {
+    updateAuthInfos: (authInfos: State["user"]) => void;
+    logout: ()=>void
+}
+
+export const useStore = create<State & Action>((set) => ({
+    user: {
+        username: "",
+        password:""
+    },
+    updateAuthInfos: (authInfos) => set((state)=> ({ ...state.user,  ...authInfos})),
+    logout: () => set(() => null)
+}))
+
