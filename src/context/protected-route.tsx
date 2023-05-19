61f4ebd6b09b1a399d9e5ff91c8cded8e19fc115
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-05-19 20:41:08 +0300

    refactor: protected route

--- src/context/protected-route.tsx
+++ src/context/protected-route.tsx
@@ -1,48 +1,25 @@
-import React, {createContext, FC, useCallback, useEffect, useMemo, useState} from 'react';
+import React, {FC, useEffect} from 'react';
 import {useRouter} from "next/navigation";
 import {HOME_ROUTE, LOGIN} from "@/constant/routes";
-import {AuthInfos} from "@/models/Auth";
+import {useStore} from "@/store/root-store";
 
-interface TUserContext {
-    user: AuthInfos;
-    updateUserInfos?: (newValue: AuthInfos) => void;
-}
-
-export const UserContext = createContext<TUserContext>({
-        user: {username: "", password: ""},
-        updateUserInfos: (user: AuthInfos) => {
-            throw Error("Not implemented")
-        }
-    }
-);
 
 interface ProtectedRouteProps {
     children: React.ReactNode;
 }
 
-const AUTH_INFOS_DEFAULT_VALUE = {username: "", password: ""};
 const ProtectedRoute: FC<ProtectedRouteProps> = ({children}) => {
-    const [user, setUser] = useState<AuthInfos>(AUTH_INFOS_DEFAULT_VALUE);
-    const {push} = useRouter()
-
-    const updateUserInfos = useCallback(function (newUser: TUserContext["user"]) {
-        setUser({...newUser});
-    }, [user]);
-
-    const userContextValue = useMemo<TUserContext>(() => ({user, updateUserInfos}), [user]);
+    const {push} = useRouter();
+    const {user} = useStore();
 
     useEffect(() => {
-        if (user.username) {
-            push(HOME_ROUTE);
-        } else {
-            push(LOGIN);
-        }
+        user.username ? push(HOME_ROUTE) : push(LOGIN);
     }, [user])
 
     return (
-        <UserContext.Provider value={userContextValue}>
+        <>
             {children}
-        </UserContext.Provider>
+        </>
     );
 };
 
