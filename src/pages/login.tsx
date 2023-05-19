8895d10130d86f86ceaf9328c4c1fa9540d2bf10
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-05-19 20:43:35 +0300

    feat: updating store when login in

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,12 +1,10 @@
-import React, {FormEvent, useContext, useState} from 'react';
+import React, {FormEvent, useState} from 'react';
 import styles from "@/styles/Login.module.css";
 import {CircularProgress} from "@chakra-ui/react";
-import {AUTH_INFOS} from "@/constant/storage";
 import {delay} from "@/utils/timing";
 import {Inter} from "next/font/google";
 import {AuthInfos} from "@/models/Auth";
 import {useStore} from "@/store/root-store"
-import {UserContext} from "@/context/protected-route";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -15,19 +13,13 @@ function Login() {
     const [authInfos, setAuthInfos] = useState<AuthInfos>(null);
     const [loading, setIsLoading] = useState(false);
     const {updateAuthInfos} = useStore();
-    const userContext = useContext(UserContext);
 
     async function handleSubmit(e: FormEvent) {
         setIsLoading(true)
         e.preventDefault();
-
-        localStorage.setItem(AUTH_INFOS, JSON.stringify(authInfos));
-        updateAuthInfos({...authInfos})
+        updateAuthInfos({...authInfos});
         await delay(2000)
-        setIsLoading(false)
-        if (userContext?.updateUserInfos) {
-            userContext?.updateUserInfos({...authInfos});
-        }
+        setIsLoading(false);
     }
 
     function handleInputChange<T>(originalFlatObj: T, attribute: keyof T) {
@@ -38,27 +30,27 @@ function Login() {
     }
 
     return (
-            <main className={`${styles.main} ${inter.className}`}>
-                <form className={styles.wrapper}>
-                    {loading && <CircularProgress value={30} color='orange' thickness='12px' isIndeterminate={true}
-                                                  position={"absolute"}/>}
-                    <label>
-                        <input type={"text"} name={"username"}
-                               className={`${styles.wrapper__input}`}
-                               onChange={handleInputChange<AuthInfos>(authInfos, "username")}
-                               placeholder={"Username or Email"}
-                        />
-                    </label>
-                    <label>
-                        <input type={"password"} name={"password"}
-                               className={`${styles.wrapper__input}`}
-                               onChange={handleInputChange(authInfos, "password")}
-                               placeholder={"Password"}
-                        />
-                    </label>
-                    <input type={"submit"} value={"Sign Up"} className={styles.submit} onClick={handleSubmit}/>
-                </form>
-            </main>
+        <main className={`${styles.main} ${inter.className}`}>
+            <form className={styles.wrapper}>
+                {loading && <CircularProgress value={30} color='orange' thickness='12px' isIndeterminate={true}
+                                              position={"absolute"}/>}
+                <label>
+                    <input type={"text"} name={"username"}
+                           className={`${styles.wrapper__input}`}
+                           onChange={handleInputChange<AuthInfos>(authInfos, "username")}
+                           placeholder={"Username or Email"}
+                    />
+                </label>
+                <label>
+                    <input type={"password"} name={"password"}
+                           className={`${styles.wrapper__input}`}
+                           onChange={handleInputChange(authInfos, "password")}
+                           placeholder={"Password"}
+                    />
+                </label>
+                <input type={"submit"} value={"Sign Up"} className={styles.submit} onClick={handleSubmit}/>
+            </form>
+        </main>
     );
 }
 
