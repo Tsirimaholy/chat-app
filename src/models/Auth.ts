fb6fdfb81db147517f1f0b9b16c844d3f87450e8
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-05-20 13:55:25 +0300

    feat: add user entity

--- src/models/Auth.ts
+++ src/models/Auth.ts
@@ -2,3 +2,20 @@ export type AuthInfos = {
     username: string;
     password: string;
 }
+
+export interface User {
+    id: number;
+    email: string;
+    mail: string;
+    bio: string;
+    name: string;
+}
+
+export interface UserProfile extends User{
+    googleId?: string;
+    status: number;
+    createdAt: string;
+    updatedAt: string;
+    deletedAt?: string;
+    token: string;
+}
