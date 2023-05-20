d654e706a8260027af21c1bfca634e16897960e3
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-05-20 12:19:43 +0300

    fix: calling api from store

--- src/store/root-store.ts
+++ src/store/root-store.ts
@@ -1,23 +1,35 @@
 import {create} from "zustand";
-import {AuthInfos} from "@/models/Auth";
+import {User} from "@/models/Auth";
 import {persist} from "zustand/middleware";
 import {AUTH_INFOS} from "@/constant/storage";
+import UserApi, {AuthUser} from "@/services/user-api";
 
 type State = {
-    user: AuthInfos
+    user: User
 }
 
 type Action = {
-    updateAuthInfos: (authInfos: State["user"]) => void;
+    logIn: (authInfos: AuthUser) => void;
     logout: () => void
 }
 
 export const useStore = create<State & Action>(persist((set) => ({
     user: {
-        username: "",
-        password: ""
+        id: null,
+        name: "",
+        bio: "",
+        email: "",
+        mail: ""
     },
-    updateAuthInfos: (authInfos) => set(() => ({user: {...authInfos}})),
+    logIn: async (authInfos) => {
+        try {
+            const user = await UserApi.logIn(authInfos);
+            set((state) => ({...state, user}));
+        } catch (e) {
+            console.error(e.message);
+            throw e;
+        }
+    },
     logout: () => set(() => ({user: {username: "", password: ""}}))
 }), {
     name: AUTH_INFOS,
