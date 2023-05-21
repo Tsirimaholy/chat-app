a7d3c71f79cf60469b30bfec19f7397877d70da1
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-05-22 00:20:01 +0300

    feat: channel creation modal

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,20 +1,46 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {useStore} from "@/store/root-store";
-import {Button, Grid, GridItem, useDisclosure} from "@chakra-ui/react";
+import {
+    Button,
+    FormControl,
+    FormLabel,
+    Grid,
+    GridItem,
+    Input,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    Spinner,
+    useDisclosure
+} from "@chakra-ui/react";
 import {AddIcon} from "@chakra-ui/icons";
 import {NavBar} from "@/components/core/nav-bar";
 import {useChannelStore} from "@/store/channel-store";
 import {ChannelList} from "@/components/core/channels-list";
+import {delay} from "@/utils/timing";
 
 function Chat() {
     const {user, logout} = useStore()
     const [menuToggle, setMenuToggle] = useState(false);
-    const {channels, getChannels} = useChannelStore();
-    const {onOpen} = useDisclosure();
+    const {channels, getChannels, createChannel} = useChannelStore();
+    const [channelsLoading, setChannelsLoading] = useState(false);
+    const [isSavingChannel, setIsSavingChannel] = useState(false);
+    const initialRef = React.useRef(null)
+    const nameRef = useRef<HTMLInputElement>();
+    const typeRef = useRef<HTMLInputElement>();
+
+    const finalRef = React.useRef(null)
+    const {onOpen, isOpen, onClose} = useDisclosure();
 
 
     useEffect(() => {
+        setChannelsLoading(true)
         getChannels()
+        setChannelsLoading(false)
     }, [])
     const toggle = () => {
         setMenuToggle(prevState => !prevState)
@@ -25,6 +51,17 @@ function Chat() {
     }
 
 
+    async function handleSaveChannel() {
+        setIsSavingChannel(true)
+        createChannel({
+            name: nameRef.current?.value || "",
+            type: typeRef.current?.value || ""
+        })
+        await delay(2000)
+        setIsSavingChannel(false);
+        onClose()
+    }
+
     return (
         <main>
             <Grid templateAreas={`"nav nav" "aside main"`}>
@@ -32,10 +69,46 @@ function Chat() {
                     <NavBar onClick={toggle} user={user} menuToggle={menuToggle} onBadgeClicked={handleLogout}/>
                 </GridItem>
                 <GridItem area={"aside"} paddingInline={5}>
+                    <>
+                        <Modal
+                            initialFocusRef={initialRef}
+                            finalFocusRef={finalRef}
+                            isOpen={isOpen}
+                            onClose={onClose}
+                        >
+                            <ModalOverlay/>
+                            <ModalContent>
+                                <ModalHeader>Create a channel</ModalHeader>
+                                <ModalCloseButton/>
+                                <ModalBody pb={6}>
+                                    <FormControl>
+                                        <FormLabel>Name</FormLabel>
+                                        <Input ref={nameRef} placeholder='channel name'/>
+                                    </FormControl>
+
+                                    <FormControl mt={4}>
+                                        <FormLabel>Type</FormLabel>
+                                        <Input ref={typeRef} placeholder='public or private'/>
+                                    </FormControl>
+                                </ModalBody>
+
+                                <ModalFooter>
+                                    <Button mr={3} colorScheme={"blue"}
+                                            onClick={handleSaveChannel}
+                                            rightIcon={isSavingChannel ? <Spinner/> : <></>}
+                                    >Save</Button>
+                                    <Button>
+                                        Cancel
+                                    </Button>
+
+                                </ModalFooter>
+                            </ModalContent>
+                        </Modal>
+                    </>
                     <Button onClick={onOpen} rightIcon={<AddIcon boxSize={3}/>}>
                         Create chanel
                     </Button>
-                    <ChannelList channels={channels}/>
+                    {channelsLoading ? <Spinner display={"block"} mt={5}/> : <ChannelList channels={channels}/>}
                 </GridItem>
                 <GridItem area={"main"}>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi aut deleniti dicta explicabo
