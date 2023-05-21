a289278e8267a645f2b7c2762c1b8980991a773b
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-05-22 00:18:56 +0300

    feat: add channel creation service

--- src/services/channel-api.ts
+++ src/services/channel-api.ts
@@ -2,9 +2,14 @@ import api from "@/services/api";
 import {Channel} from "@/models/Channel";
 
 class ChannelApi {
-    async getAll(){
+    async getAll() {
         const {data} = await api.get("/channels");
         return data?.channels as Channel[];
+    }
+
+    async createChannel(channel: Channel) {
+        const {data} = await api.post("/channel", {...channel, members: []});
+        return data?.channel as Channel;
     }
 }
 
