84546bf51813d06cff3725c90e4bfd692ab3733a
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-05-30 20:07:30 +0300

    feat: loading page while waiting for redirection

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import {GridItem} from "@chakra-ui/react";
+import React, {useEffect} from 'react';
+import {GridItem, Heading, Spinner} from "@chakra-ui/react";
 import Layout from "@/pages/_layout";
+import {useRouter} from "next/navigation";
+import {CHANNEL} from "@/constant/routes";
 
 function Chat() {
+    const {push} = useRouter();
+    useEffect(() => {
+        // Todo maybe verify if there is channel before
+        push(`${CHANNEL}/${1}`);
+    },[push])
     return (
         <main>
             <Layout>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi aut deleniti dicta explicabo
-                    fuga
-                    ipsa, itaque obcaecati pariatur, sed tempore ut? At ex, hic incidunt nesciunt pariatur rerum
-                    sint?
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi aut deleniti dicta explicabo
-                    fuga
-                    ipsa, itaque obcaecati pariatur, sed tempore ut? At ex, hic incidunt nesciunt pariatur rerum
-                    sint?
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi aut deleniti dicta explicabo
-                    fuga
-                    ipsa, itaque obcaecati pariatur, sed tempore ut? At ex, hic incidunt nesciunt pariatur rerum
-                    sint?
+                   <Heading>Loading</Heading>
+                    <Spinner/>
             </Layout>
 
         </main>);
