d034a4be4fc7deee4f2833927a8dddb0b785d8e0
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-05-30 20:01:43 +0300

    feat: showing channel header

--- src/pages/channel/[channelId].tsx
+++ src/pages/channel/[channelId].tsx
@@ -1,21 +1,32 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Layout from "@/pages/_layout";
 import {useRouter} from "next/router";
 import useMessageStore from "@/store/message-store";
 import {MessageList} from "@/components/core/Message/message-list";
+import {useChannelStore} from "@/store/channel-store";
+import {Channel} from "@/models/Channel";
+import {ChannelHeader} from "@/pages/channel/component/_ChannelHeading";
+
 
 function ChannelMessages() {
     const {query} = useRouter();
-    console.info(query)
     const id = Number.parseInt(query["channelId"] as unknown as string);
+
+    const [currentChannel, setCurrentChannel] = useState<Channel>();
     const {messages, getMessagesByChannel} = useMessageStore();
+    const {getChannelById} = useChannelStore();
+
     useEffect(() => {
-        getMessagesByChannel(id).catch(reason => console.error(reason))
-    }, [getMessagesByChannel, id])
+        (async () => {
+            const [_, channel] = await Promise.all([getMessagesByChannel(id), getChannelById(id)]);
+            setCurrentChannel(channel);
+        })()
+    }, [getChannelById, getMessagesByChannel, id])
 
 
     return (
         <Layout>
+            <ChannelHeader currentChannel={currentChannel}/>
             <MessageList messages={messages}/>
         </Layout>
     );
