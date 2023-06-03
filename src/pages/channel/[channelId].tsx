d84c7bc9ac1972f77096f63f0589c7c4fd4e002c
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-06-03 11:21:34 +0300

    refactor: extracting message field

--- src/pages/channel/[channelId].tsx
+++ src/pages/channel/[channelId].tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Layout from "@/pages/_layout";
 import {useRouter} from "next/router";
 import useMessageStore from "@/store/message-store";
@@ -6,6 +6,8 @@ import {MessageList} from "@/components/core/Message/message-list";
 import {useChannelStore} from "@/store/channel-store";
 import {Channel} from "@/models/Channel";
 import {ChannelHeader} from "@/pages/channel/component/_ChannelHeading";
+import {MessageField} from "@/pages/channel/component/message-field";
+
 
 
 function ChannelMessages() {
@@ -13,21 +15,32 @@ function ChannelMessages() {
     const id = Number.parseInt(query["channelId"] as unknown as string);
 
     const [currentChannel, setCurrentChannel] = useState<Channel>();
-    const {messages, getMessagesByChannel} = useMessageStore();
+    const {messages, getMessagesByChannel, sendMessage} = useMessageStore();
     const {getChannelById} = useChannelStore();
-
+    const inputRef = useRef<HTMLInputElement>(null);
     useEffect(() => {
         (async () => {
             const [_, channel] = await Promise.all([getMessagesByChannel(id), getChannelById(id)]);
             setCurrentChannel(channel);
         })()
     }, [getChannelById, getMessagesByChannel, id])
-
+    useEffect(() => {
+        inputRef?.current?.focus();
+    }, [id])
+    const handleSendMessage = async (messageContent: string) => {
+        await sendMessage(id, {recipientId: 1, channelId: id, content: messageContent});
+        // clear input value
+        if (inputRef?.current?.value) inputRef.current.value = "";
+    }
 
     return (
         <Layout>
             <ChannelHeader currentChannel={currentChannel}/>
             <MessageList messages={messages}/>
+            <MessageField ref={inputRef} onKeyDown={async event => {
+                if (event?.key == 'Enter' && inputRef?.current?.value) await handleSendMessage(inputRef.current.value)
+            }}
+                          onClick={() => handleSendMessage(inputRef?.current?.value || ' ')}/>
         </Layout>
     );
 }
---
+++ src/pages/channel/component/message-field.tsx
@@ -0,0 +1,31 @@
+import React, {forwardRef} from "react";
+import {InputGroup, InputRightElement} from "@chakra-ui/input";
+import {Box, IconButton, Input} from "@chakra-ui/react";
+
+type MessageFieldProps = {
+    onKeyDown: (event: React.KeyboardEvent<HTMLInputElement> | undefined) => Promise<void>,
+    onClick: () => Promise<void>
+};
+type Ref = HTMLInputElement;
+export const MessageField = forwardRef<Ref, MessageFieldProps>((props, ref) => {
+    return <InputGroup size="md" pos={"sticky"} bottom={0} left={0} right={0} marginTop={"3em"}>
+        <Input
+            pr="4.5rem"
+            type={"show"}
+            placeholder="Enter password"
+            ref={ref}
+            onKeyDown={props.onKeyDown}
+        />
+        <Box>
+            <InputRightElement width="4.5rem"
+                               onClick={props.onClick}>
+                <IconButton h="1.75rem" size="sm" icon={<span className={"fa fa-send"}/>}
+                            aria-label={"send button"}
+                />
+            </InputRightElement>
+        </Box>
+
+    </InputGroup>;
+})
+
+MessageField.displayName="MessageField";
