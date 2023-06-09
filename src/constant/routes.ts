2bd30f181785413e2bb5987268154fd983c1b30e
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-06-09 21:03:18 +0300

    feat: add channel creation route constant

--- src/constant/routes.ts
+++ src/constant/routes.ts
@@ -4,4 +4,5 @@ export const SIGN_UP = "/sign-up";
 export const PROFILE_ROUTE = "/profile"
 export const HOME_ROUTE = "/chat"
 export const CHANNEL = "/channel"
+export const CREATE_CHANNEL_ROUTE = CHANNEL+"/create"
 export const ROOT_ROUTE = "/";
