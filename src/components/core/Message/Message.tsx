9a2188493d37182415317eb8118ef570071ffdb4
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-06-09 17:23:07 +0300

    feat: add date and sender name

--- src/components/core/Message/Message.tsx
+++ src/components/core/Message/Message.tsx
@@ -1,25 +1,41 @@
-import {Avatar, HStack, ListItem} from "@chakra-ui/react";
+import {Avatar, HStack, ListItem, VStack, Text, Box} from "@chakra-ui/react";
 import React from "react";
 import {TMessage} from "@/models/TMessage";
 
 type MessageProps = { message: TMessage };
 
 export function Message(props: MessageProps) {
-    const { message: {content, sender}} = props;
+    const {message: {content, sender, createdAt}} = props;
+    const createdDate = new Date(createdAt);
+    const today = new Date();
+    let parsedDateTime = createdDate.toDateString();
+
+    const isToday = today.getDay() == createdDate.getDay() && today.getMonth() == createdDate.getMonth() && today.getFullYear() == createdDate.getFullYear();
+    if (isToday) {
+        parsedDateTime = createdDate.toLocaleTimeString();
+    }
+
+
     return (
         <HStack
-                >
-            <Avatar size={"md"} name={sender.name}
+            alignItems={"flex-start"} justifyContent={'left'} mb={'3'}>
+            <Avatar size={"sm"} name={sender.name}
                     bg={'gray.600'}
                     colorScheme={"blackAlpha"}
                     color={"white"}
                     marginX={"2"}
                     marginRight={'0'}
             />
-            <ListItem p={"2"} backgroundColor={"blackAlpha.500"}
-                      borderRadius='md'>
-                {content}
-            </ListItem>
+            <VStack alignItems={'flex-start'}  backgroundColor={"blackAlpha.500"} p={"2"} borderRadius={'sm'}>
+                <Box>
+                    <Text as={'b'} mr={2}>{sender.name}</Text>
+                    <Text fontSize={'sm'} display={'inline'}>{parsedDateTime}</Text>
+                </Box>
+                <ListItem>
+                    <Text>{content}</Text>
+                </ListItem>
+            </VStack>
+
         </HStack>
     )
 }
